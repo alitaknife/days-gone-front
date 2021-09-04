0a666d3e62bb94c18e2041c178d80c2bd282e6c6
Alita <alitaknife@gmail.com>
2021-09-04 23:59:17 +0800

    更改上线配置

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
@@ -37,7 +37,7 @@ module.exports = {
       errors: true
     },
     proxy: {
-      '/': {
+      '/api': {
         target: 'http://localhost:8199',
         changeOrigin: true,
       },
