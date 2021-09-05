8a63a2b324e4c3f07e87fa059832a606349300ec
Alita <alitaknife@gmail.com>
2021-09-05 13:09:05 +0800

    logo，路由信息修改

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,7 @@ export const constantRoutes = [
                 path: 'dashboard',
                 name: 'Dashboard',
                 component: () => import('@/views/dashboard/index'),
-                meta: { title: '主页', icon: 'dashboard' }
+                meta: { title: '首页', icon: 'dashboard' }
             }
         ]
     },
